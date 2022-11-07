6a8a093c5b30bef72a2db4c0e87bad73fc13f8f4
Pavel Golovin <pau.golovin@gmail.com>
2022-11-08 00:47:54 +0400

    first commit

---
+++ AeroCalc.cs
@@ -0,0 +1,254 @@
+﻿using AeroCalc.PanelMethodSolver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using AeroCalc.ResultSaver;
+
+namespace AeroCalc
+{
+    public partial class AeroCalc : Form
+    {
+        public AeroCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            fileTextBox.Text = path;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string path = fileTextBox.Text;
+            string Ratio = ratioTextBox.Text;
+            string Taper = taperTextBox.Text;
+            string Sweep = sweepTextBox.Text;
+            string FirstAoA = firstAoATextBox.Text;
+            string LastAoA = lastAoATextBox.Text;
+            string StepAoA = stepTextBox.Text;
+            string XMeshSize = XMeshSizeTextBox.Text;
+            string ZMeshSize = ZMeshSizeTextFile.Text;
+
+            double ratio = 0;
+            if (!double.TryParse(Ratio, out ratio))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+            ratio = Math.Abs(ratio);
+            if (ratio == 0)
+            {
+                MessageBox.Show("Error. Ratio must be more than zero!\nTry again)");
+                Application.Exit();
+            }
+
+            double taper = 0;
+            if (!double.TryParse(Taper, out taper))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+            taper = Math.Abs(taper);
+            if (taper == 0)
+            {
+                MessageBox.Show("Error. Taper must be more than zero!\nTry again)");
+                Application.Exit();
+            }
+
+            double sweep = 0;
+            if (!double.TryParse(Sweep, out sweep))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+
+            double firstAoA = 0;
+            if (!double.TryParse(FirstAoA, out firstAoA))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+
+            double lastAoA = 0;
+            if (!double.TryParse(LastAoA, out lastAoA))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+
+            double stepAoA = 0;
+            if (!double.TryParse(StepAoA, out stepAoA))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+            stepAoA = Math.Abs(stepAoA);
+            if (stepAoA == 0)
+            {
+                stepAoA = 1;
+            }
+
+            if (lastAoA < firstAoA)
+            {
+                double swapAoA = firstAoA;
+                firstAoA = lastAoA;
+                lastAoA = swapAoA;
+            }
+
+            int xMeshSize = 0;
+            if(!int.TryParse(XMeshSize, out xMeshSize))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+            xMeshSize = Math.Abs(xMeshSize);
+            if (xMeshSize == 0)
+            {
+                MessageBox.Show("Error. Mesh size along X axis must be more than zero!\nTry again)");
+                Application.Exit();
+            }
+
+            int zMeshSize = 0;
+            if (!int.TryParse(ZMeshSize, out zMeshSize))
+            {
+                Console.WriteLine("Error. Error in the input data");
+                MessageBox.Show("Error. Error in the input data");
+                Console.ReadLine();
+                Application.Exit();
+            }
+            zMeshSize = Math.Abs(zMeshSize);
+            zMeshSize = Math.Abs(xMeshSize);
+            if (zMeshSize == 0)
+            {
+                MessageBox.Show("Error. Mesh size along Z axis must be more than zero!\nTry again)");
+                Application.Exit();
+            }
+
+            //resultTextBox.Text = "AoA" + "\t" + "Cy" + "\t" + "Cx" + Environment.NewLine;
+            string result = "AoA" + "\t" + "Cy" + "\t" + "Cx" + Environment.NewLine;
+            resultTextBox.Text = result;
+
+            for (double i = firstAoA; i <= lastAoA; i += stepAoA)
+            {
+                (double Cy, double Cx) = TCyCxSolver.GetCyCx(path, xMeshSize, zMeshSize, ratio, taper, sweep, i);
+                //resultTextBox.Text = resultTextBox.Text + Math.Round(i, 5) + "\t" + Math.Round(Cy, 5) + "\t" + Math.Round(Cx, 5) + Environment.NewLine;
+                result += Math.Round(i, 5) + "\t" + Math.Round(Cy, 5) + "\t" + Math.Round(Cx, 5) + Environment.NewLine;
+                resultTextBox.Text = result;
+            }
+            result += Environment.NewLine + Environment.NewLine + "Succes!";
+            resultTextBox.Text = result;
+        }
+
+        private void ratioTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            ratioTextBox.Clear();
+        }
+
+        private void fileTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            fileTextBox.Clear();
+        }
+
+        private void taperTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            taperTextBox.Clear();
+        }
+
+        private void sweepTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            sweepTextBox.Clear();
+        }
+
+        private void firstAoATextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            firstAoATextBox.Clear();
+        }
+
+        private void lastAoATextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            lastAoATextBox.Clear();
+        }
+
+        private void stepTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            stepTextBox.Clear();
+        }
+
+        private void XMeshSizeTextBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            XMeshSizeTextBox.Clear();
+        }
+
+        private void ZMeshSizeTextFile_MouseClick(object sender, MouseEventArgs e)
+        {
+            ZMeshSizeTextFile.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            if (resultTextBox.Text == null)
+            {
+                MessageBox.Show("The calculation was not carried out. Make a calculation.");
+            }
+            else
+            {
+                int ResultLines = resultTextBox.Lines.Length - 3;
+
+                string path = fileTextBox.Text;
+
+                string path_result;
+
+                if (Path.GetExtension(path) == ".xlsx")
+                {
+                    path_result = path.Substring(0, path.Length - 5) + "_result.xlsx";
+                }
+                else
+                {
+                    path_result = path.Substring(0, path.Length - 4) + "_result.xlsx";
+                }
+
+                List<string> result_strings = new List<string>();
+
+                for (int i = 0; i < ResultLines; i++)
+                {
+                    string[] line = resultTextBox.Lines[i].Split("\t");
+                    result_strings.Add(line[0]);
+                    result_strings.Add(line[1]);
+                    result_strings.Add(line[2]);
+                }
+
+                TResultSaver.SaveResult(path_result, result_strings);
+                MessageBox.Show("File " + path_result + " Successfully created!!!");
+            }
+        }
+    }
+}
---
+++ PanelMethodSolver/TCyCxSolver.cs
@@ -0,0 +1,96 @@
+﻿using AeroCalc.MeshGenerator;
+using AeroCalc.Panel;
+using AeroCalc.WingReader;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeroCalc.PanelMethodSolver
+{
+    public class TCyCxSolver
+    {
+        /// <summary>
+        /// Here Lift Coefficient Cy and Drag Coefficient Cx will be found
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="XMeshSize"></param>
+        /// <param name="ZMeshSize"></param>
+        /// <param name="Ratio"></param>
+        /// <param name="Taper"></param>
+        /// <param name="Sweep"></param>
+        /// <param name="AoA"></param>
+        /// <returns></returns>
+        public static Tuple<double, double> GetCyCx(string path, int XMeshSize, int ZMeshSize, double Ratio, double Taper, double Sweep, double AoA)
+        {
+            double Cy = 0;
+            double Cx = 0;
+            
+            // Read the Wing
+            double[,] ReadWing = TWingReader.GetNodes(path);
+
+            // Generate the Mesh
+            List<TPanel> MeshPanels = TMeshGenerator.MeshGenerator(ReadWing, XMeshSize, ZMeshSize, Ratio, Taper, Sweep, AoA);
+
+            // Get Cp
+            double[] Cp = TCpSolver.GetCp(path, XMeshSize, ZMeshSize, Ratio, Taper, Sweep, AoA);
+
+
+            double TopSurfaceCp = 0;
+            double BottomSurfaceCp = 0;
+
+            // Find Wing Planned Projection Area
+            double PlannedProjectionArea = 0;
+
+            for (int i = 0; i < Cp.Length; i++)
+            {
+                TPanel PlanPanel = new TPanel(new Vector3(MeshPanels[i].PanelPoint_1.X, 0, MeshPanels[i].PanelPoint_1.Z),
+                                              new Vector3(MeshPanels[i].PanelPoint_2.X, 0, MeshPanels[i].PanelPoint_2.Z),
+                                              new Vector3(MeshPanels[i].PanelPoint_3.X, 0, MeshPanels[i].PanelPoint_3.Z),
+                                              new Vector3(MeshPanels[i].PanelPoint_4.X, 0, MeshPanels[i].PanelPoint_4.Z));
+                PlannedProjectionArea += PlanPanel.Area;
+            }
+
+            PlannedProjectionArea /= 2;
+
+            // Find pressure coefficient for top and bottom wing surface 
+            for (int i = 0; i < ZMeshSize; i++)
+            {
+                for (int j = 0; j < XMeshSize; j++)
+                {
+                    BottomSurfaceCp += Cp[i * 2 * XMeshSize + j] * MeshPanels[i * 2 * XMeshSize + j].Area / PlannedProjectionArea;
+                    TopSurfaceCp += Cp[(i * 2 + 1) * XMeshSize + j] * MeshPanels[(i * 2 + 1) * XMeshSize + j].Area / PlannedProjectionArea;
+                }
+            }
+
+            // Get Cy
+            Cy = BottomSurfaceCp - TopSurfaceCp;
+            if (Math.Abs(Cy) < 0.0001)
+                Cy = 0;
+
+            // Find full wing area
+            double FullArea = 0;
+            for (int i = 0; i < MeshPanels.Count; i++)
+            {
+                FullArea += MeshPanels[i].Area;
+            }
+
+            // Find Drag Coefficien when Y = 0 Cx0
+            double Cx0 = 0.003 * FullArea / PlannedProjectionArea;
+
+            // Find drag-due-to-lift factor A
+            double A = 1 / (Math.PI * Ratio);
+
+            // Get Cx
+            Cx = Cx0 + A * Math.Pow(Cy, 2);
+
+            Tuple<double, double> CyCx = new Tuple<double, double>(Cy, Cx);
+
+            return CyCx;
+        }
+
+    }
+}
